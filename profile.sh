c0bdb9feaf841776251fed97341ca8966ac8ab09
Seph Gentle <me@josephg.com>
2024-04-21 19:38:07 +1000

    Added profiling profile to Cargo.toml and added simple profile.sh script

---
+++ profile.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+RUSTFLAGS="-Cforce-frame-pointers=yes" cargo build --profile profiling
+#perf record -g -F 9999 --call-graph fp target/profiling/examples/profile
+perf record -g -F 99 --call-graph fp target/profiling/examples/profile
+perf script -F +pid > /tmp/test.perf
+
+echo "Perf data in perf.data and script in /tmp/test.perf"
