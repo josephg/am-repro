3979188c6b57cc01dc493e994949bd0cb2da3ee4
Seph Gentle <me@josephg.com>
2024-04-24 10:54:07 +1000

    Fixed profiling script

--- profile.sh
+++ profile.sh
@@ -3,7 +3,7 @@ set -e
 
 RUSTFLAGS="-Cforce-frame-pointers=yes" cargo build --profile profiling
 #perf record -g -F 9999 --call-graph fp target/profiling/examples/profile
-perf record -g -F 99 --call-graph fp target/profiling/examples/profile
+perf record -g -F 99 --call-graph fp target/profiling/am-repro
 perf script -F +pid > /tmp/test.perf
 
 echo "Perf data in perf.data and script in /tmp/test.perf"
